c99c01deec6d8564500d6b6ba96ef89341c6c59e
Dzinious <99750838+Dzinious@users.noreply.github.com>
2025-11-19 21:44:30 +0200

    replace mock contact information with data from backend

--- frontend/src/pages/Checkout.tsx
+++ frontend/src/pages/Checkout.tsx
@@ -3,12 +3,34 @@ import { storage } from '../utils/StorageService';
 import LiqPayButton from '../components/order/LiqPayButton';
 import { useNavigate } from 'react-router-dom';
 import { Truck, Box, Zap, CreditCard, DollarSign } from 'lucide-react';
+import {useGetUserByTokenQuery} from '../api/userInfoApi.ts';
+import { Address } from '../models/Address.ts';
+import { CartItem } from '../models/CartItem.ts';
+import LoadingMessage from '../components/common/LoadingMessage.tsx';
+import ErrorMessage from '../components/common/ErrorMessage.tsx';
 
 const Checkout: React.FC = () => {
   const navigate = useNavigate();
-  const [cart, setCart] = useState<any[]>([]);
+  const [cart, setCart] = useState<CartItem[]>([]);
   const [delivery, setDelivery] = useState<string>('courier');
   const [payment, setPayment] = useState<string>('card');
+
+  const token = storage.getAccessToken();
+  const {data: user, isLoading, isError, refetch} = useGetUserByTokenQuery(token ?? '', {skip: !token});
+
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+
+  const [deliveryAddress, setDeliveryAddress] = useState<Address>(
+  {
+    city: "",
+    street: "",
+    house: "",
+    apartments: ""
+  }
+);
 
   useEffect(() => {
     setCart(storage.getGuestCart());
@@ -30,6 +52,27 @@ const Checkout: React.FC = () => {
     }
   };
 
+   useEffect(() => {
+    if (user) {
+      setFirstName(user.firstName);
+      setLastName(user.lastName);
+      setEmail(user.email);
+      setPhone(user.phoneNumber)
+
+      if(user?.addresses?.[0]) {
+        setDeliveryAddress(user?.addresses?.[0])
+      }
+    }
+  }, [user]);
+
+  if (isLoading) {
+    return LoadingMessage("checkout page");
+  }
+  
+  if (isError) {
+    return ErrorMessage("error loading checkout page", "couldn't retrieve data from the database")
+  }
+
   return (
     <div className="container mx-auto p-6">
       <h1 className="text-4xl font-bold text-center text-blue-600 mb-8">Checkout</h1>
@@ -38,14 +81,18 @@ const Checkout: React.FC = () => {
           <section className="bg-white rounded-lg p-6 border">
             <h2 className="text-lg font-medium mb-4">Shipping Information</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-              <input placeholder="First Name" className="p-3 rounded-lg bg-gray-100" />
-              <input placeholder="Last Name" className="p-3 rounded-lg bg-gray-100" />
-              <input placeholder="Email" className="p-3 rounded-lg bg-gray-100 md:col-span-2" />
-              <input placeholder="Phone Number" className="p-3 rounded-lg bg-gray-100 md:col-span-2" />
-              <input placeholder="City" className="p-3 rounded-lg bg-gray-100" />
-              <input placeholder="Street" className="p-3 rounded-lg bg-gray-100" />
-              <input placeholder="House" className="p-3 rounded-lg bg-gray-100" />
-              <input placeholder="Apartment (optional)" className="p-3 rounded-lg bg-gray-100" />
+              <input placeholder="First Name" className="p-3 rounded-lg bg-gray-100" value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
+              <input placeholder="Last Name" className="p-3 rounded-lg bg-gray-100" value={lastName} onChange={(e) => setLastName(e.target.value)}/>
+              <input placeholder="Email" className="p-3 rounded-lg bg-gray-100 md:col-span-2" value={email} onChange={(e) => setEmail(e.target.value)}/>
+              <input placeholder="Phone Number" className="p-3 rounded-lg bg-gray-100 md:col-span-2" value={phone} onChange={(e) => setPhone(e.target.value)}/>
+              <input placeholder="City" className="p-3 rounded-lg bg-gray-100" value={deliveryAddress.city} 
+                     onChange={(e) => setDeliveryAddress(prev => ({...prev, city: e.target.value}))}/>
+              <input placeholder="Street" className="p-3 rounded-lg bg-gray-100" value={deliveryAddress.street}
+                     onChange={(e) => setDeliveryAddress(prev => ({...prev, street: e.target.value}))}/>
+              <input placeholder="House" className="p-3 rounded-lg bg-gray-100" value={deliveryAddress.house} 
+                     onChange={(e) => setDeliveryAddress(prev => ({...prev, house: e.target.value}))}/>
+              <input placeholder="Apartment (optional)" className="p-3 rounded-lg bg-gray-100" value={deliveryAddress.apartments} 
+                     onChange={(e) => setDeliveryAddress(prev => ({...prev, apartments: e.target.value}))}/>
             </div>
           </section>
 
