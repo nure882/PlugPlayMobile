21054d91e36d109f02ea140a2bfc85e68e6bbb52
Dzinious <99750838+Dzinious@users.noreply.github.com>
2025-11-21 12:55:00 +0200

    add validation check for empty cart

--- frontend/src/pages/Checkout.tsx
+++ frontend/src/pages/Checkout.tsx
@@ -158,6 +158,11 @@ const Checkout: React.FC = () => {
       return false;
     }
 
+    if(enrichedItems.length === 0) {
+      setError("Cannot place an order with empty cart, please add some products");
+      return false;
+    }
+
     setError(null);
     return true;
   };
