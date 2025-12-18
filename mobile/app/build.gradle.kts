c7f43d001165e9951a3cf8130a5e523bb890a513
Soboliev Maksym <maksym.soboliev@nure.ua>
2025-12-18 13:11:38 +0200

    fix package conflict, causing "ConfirmCore" in label

--- mobile/app/build.gradle.kts
+++ mobile/app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "com.plugplay.plugplaymobile"
         minSdk = 28
         targetSdk = 35
-        versionCode = 2
+        versionCode = 5
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
